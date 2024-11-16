f2003c4eb35c6fc5ffc933be7fcc201a51c98254
prxneeth <praneethreddyg1818@gmail.com>
2024-11-16 15:57:04 +0530

    appwrite config redux setup

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import "./App.css";
+import { Footer, Header } from "./components";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { login, logout } from "./store/authSlice";
+import authService from "./appwrite/auth";
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main></main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
+}
+
+export default App;
---
+++ src/appwrite/auth.js
@@ -0,0 +1,61 @@
+import conf from "../conf/conf";
+
+import { Client, Account, ID } from "appwrite";
+
+class AuthService {
+    client = new Client();
+    account;
+
+    constructor() {
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+        this.account = new Account(this.client)
+    }
+
+    async createAccount({ email, password, name }) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name)
+            if (userAccount) {
+                // login method
+                return this.login({ email, password })
+            } else {
+                return userAccount
+            }
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async login({ email, password }) {
+        try {
+            await this.account.createEmailPasswordSession(email, password)
+        } catch (error) {
+            throw error
+        }
+    }
+
+
+    async getCurrentUser() {
+        try {
+            return await this.account.get()
+        } catch (error) {
+            console.log("appwrite service error", error)
+
+        }
+        return null
+    }
+
+    async logout() {
+        try {
+            return await this.account.deleteSessions()
+        } catch (error) {
+            console.log("appwrite service logout error", error)
+
+        }
+    }
+}
+
+const authService = new AuthService()
+
+export default authService
